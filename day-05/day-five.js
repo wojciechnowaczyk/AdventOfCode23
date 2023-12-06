b67e6d84177676c04787ff7f1fd88dca2dc0009d
Wojtek <wojciech.nowaczyk@gmail.com>
2023-12-06 21:32:09 +0100

    Seeds were extracted

--- day-05/day-five.js
+++ day-05/day-five.js
@@ -2,7 +2,18 @@ const events = require('events');
 const fs = require('fs');
 const readline = require('readline');
 
-let sum= 0;
+let locations = [];
+
+let seeds = [];
+let seed_to_soil_map =[];
+let soil_to_fetilizer_map = [];
+let fertilizer_to_water_map = [];
+let water_to_light_map = [];
+let light_to_temperature_map = [];
+let temperature_to_humidity_map =[];
+let humidity_to_location_map = [];
+
+let currentIterationName = "";
 
 (async function processLineByLine() {
   try {
@@ -12,19 +23,29 @@ let sum= 0;
     });
 
     rl.on('line', (line) => {
-      calculateData(line)
+      parseData(line)
     });
 
     await events.once(rl, 'close');
 
     console.log('Reading file line by line with readline done.');
-    console.log("sum: " + sum)
   } catch (err) {
     console.error(err);
   }
 })();
 
 
-const calculateData = (dataLine) => {
-
-}
+const parseData = (dataLine) => {
+  //check if line is a string line
+  if(!dataLine.match(/^[0-9\s]+$/)){
+    //get the string before :
+    const title= dataLine.split(':')[0]
+    console.log(title)
+    if(title === 'seeds'){
+      let seedsFromData = dataLine.split(':')[1];
+      seeds = seedsFromData.split(' ').filter(Boolean)
+    }
+    console.log(seeds);
+  }
+  //set string line as current iteration
+  }
