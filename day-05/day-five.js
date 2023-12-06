0308b9f98346a354eee66e5194bf409d34ac500b
Wojtek <wojciech.nowaczyk@gmail.com>
2023-12-06 15:27:53 +0100

    day five init commit

---
+++ day-05/day-five.js
@@ -0,0 +1,30 @@
+const events = require('events');
+const fs = require('fs');
+const readline = require('readline');
+
+let sum= 0;
+
+(async function processLineByLine() {
+  try {
+    const rl = readline.createInterface({
+      input: fs.createReadStream('./input-05.txt'),
+      crlfDelay: Infinity
+    });
+
+    rl.on('line', (line) => {
+      calculateData(line)
+    });
+
+    await events.once(rl, 'close');
+
+    console.log('Reading file line by line with readline done.');
+    console.log("sum: " + sum)
+  } catch (err) {
+    console.error(err);
+  }
+})();
+
+
+const calculateData = (dataLine) => {
+
+}
